7479d2421ce47911f69f867106773eba13cb07b4
sureyya <sureyyaamac@gmail.com>
2022-05-17 12:37:59 +0300

    middleware

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express= require('express');
+const path=require('path');
 const app=express();
+
+//Middlewares
+app.use(express.static('public'));
+
 const port =3000;
 app.get('/',(req,res)=>{
-    const photo={
-        id:1,
-        name: "photo",
-        description:"description"
-    }
-    res.send(photo);
+    res.sendFile(path.resolve(__dirname,"temp/index.html"))
 })
 app.listen(port,()=>{
     console.log(`Server started at ${port}`);
